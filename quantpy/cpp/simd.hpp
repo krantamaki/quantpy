acf74ae08c1e238e67ad2ea2894e17fc0df6326f
krantamaki <kasper.rantamaki@gmail.com>
2026-01-11 10:44:07 +0200

    Adding quantpy namespace to all files

--- quantpy/cpp/simd.hpp
+++ quantpy/cpp/simd.hpp
@@ -16,46 +16,50 @@
 #define SIMD_SIZE 32  // Bytes
 
 
-namespace simd {
+namespace quantpy {
+
+  namespace simd {
 
 
-  /**
-   * @struct simd
-   * @brief Templated struct for SIMD vectors
-   * @tparam T  The type held in the SIMD vector
-   * @var simd::data The actual SIMD vector held within the struct
-   */
-  template <typename T>
-  struct simd {
+    /**
+     * @struct simd
+     * @brief Templated struct for SIMD vectors
+     * @tparam T  The type held in the SIMD vector
+     * @var simd::data The actual SIMD vector held within the struct
+     */
+    template <typename T>
+    struct simd {
 
-    typedef T data __attribute__ ((__vector_size__ (SIMD_SIZE)));
+      typedef T data __attribute__ ((__vector_size__ (SIMD_SIZE)));
 
-  };
+    };
 
 
-  /**
-   * @brief Template function that chooses either the vectorized or non-vectorized datatype based on a boolean flag.
-   * @details Does the choice at compile time and is thus similar to std::conditional_t, but doesn't evaluate the 
-   * unchosen option.
-   * @tparam T          The type held in the SIMD vector
-   * @tparam vectorize  Boolean flag telling if a SIMD vector type is to be returned
-   * @returns           Uninitialized instance of the type T or a SIMD vector holding type T depending on the 
-   * value of the 'vectorize' template parameter
-   */
-  template <typename T, bool vectorize>
-  auto choose_simd() {
+    /**
+     * @brief Template function that chooses either the vectorized or non-vectorized datatype based on a boolean flag.
+     * @details Does the choice at compile time and is thus similar to std::conditional_t, but doesn't evaluate the 
+     * unchosen option.
+     * @tparam T          The type held in the SIMD vector
+     * @tparam vectorize  Boolean flag telling if a SIMD vector type is to be returned
+     * @returns           Uninitialized instance of the type T or a SIMD vector holding type T depending on the 
+     * value of the 'vectorize' template parameter
+     */
+    template <typename T, bool vectorize>
+    auto choose_simd() {
 
-    if constexpr (vectorize) {
-      typename simd<T>::data a;
-      return a;
+      if constexpr (vectorize) {
+        typename simd<T>::data a;
+        return a;
+      }
+      else {
+        T a;
+        return a;
+      }
+
     }
-    else {
-      T a;
-      return a;
-    }
+
 
   }
-
 
 }
 
--- quantpy/cpp/sutils.hpp
+++ quantpy/cpp/sutils.hpp
@@ -18,129 +18,133 @@
 #include "logging.hpp"
 
 
-namespace sutils {
+namespace quantpy {
+
+  namespace sutils {
 
 
-  /**
-   * @brief Function that splits a string by a wanted (character) delimeter.
-   * @details Doesn't count empty strings as tokens
-   * @param str    Constant string pointer to the string to be split
-   * @param delim  The delimeter by which the string is split. Defaults to a single space ' '
-   * @returns      A std::vector object with the string parts
-   */ 
-  inline std::vector<std::string> split(const std::string& str, char delim = ' ') {
+    /**
+     * @brief Function that splits a string by a wanted (character) delimeter.
+     * @details Doesn't count empty strings as tokens
+     * @param str    Constant string pointer to the string to be split
+     * @param delim  The delimeter by which the string is split. Defaults to a single space ' '
+     * @returns      A std::vector object with the string parts
+     */ 
+    inline std::vector<std::string> split(const std::string& str, char delim = ' ') {
 
-    std::istringstream stream(str);
-    std::string token;
+      std::istringstream stream(str);
+      std::string token;
 
-    std::vector<std::string> ret;
+      std::vector<std::string> ret;
 
-    while (std::getline(stream, token, delim)) {
-      if (token != "") {
-        ret.push_back(token);
+      while (std::getline(stream, token, delim)) {
+        if (token != "") {
+          ret.push_back(token);
+        }
       }
+
+      return ret;
+
     }
 
-    return ret;
+
+    /**
+     * @brief Function that joins multiple strings together by a delimeter
+     * @param strs   A std::vector of the strings to be joined
+     * @param delim  The delimeter with which the string is joined. Defaults to a single space ' '
+     * @returns      A std::string with the strings joined with the delimeter
+     */
+    inline std::string join(const std::vector<std::string> strs, char delim=' ') {
+
+      std::ostringstream stream;
+
+      for (const std::string str : strs) {
+        stream << delim << str;
+      }
+
+      return stream.str();
+
+    }
+
+
+    /**
+     * @brief Function that converts a given string to lower case
+     * @param str  Constant string pointer to the string to be lower cased
+     * @returns    A std::string object equivalent to the given string in lower case
+     */
+    inline std::string toLower(const std::string& str) {
+
+      std::string ret;
+      ret.reserve(str.size());
+      for (int i = 0; i < (int)str.size(); i++) {
+        char c = (char)tolower(str[i]);
+        ret.push_back(c);
+      }
+
+      return ret;
+
+    } 
+
+
+    /**
+     * @brief Function that converts a given string to upper case
+     * @param str  Constant string pointer to the string to be upper cased
+     * @returns    A std::string object equivalent to the given string in upper case
+     */
+    inline std::string toUpper(const std::string& str) {
+
+      std::string ret;
+      ret.reserve(str.size());
+      for (int i = 0; i < (int)str.size(); i++) {
+        char c = (char)toupper(str[i]);
+        ret.push_back(c);
+      }
+
+      return ret;
+
+    } 
+
+
+    /**
+     * @brief Function that removes the leading and trailing whitespaces from a string
+     * @param str  Constant string pointer to the string to be trimmed
+     * @returns    A std::string object equivalent to the given string without leading and trailing whitespaces
+     */
+    inline std::string trim(const std::string& str) {
+
+      if (str == "") {
+        ERROR("Cannot trim an empty string!");
+      }
+
+      // Find the index of the first non-whitespace character
+      int start = -1;
+      for (int i = 0; i < (int)str.size(); i++) {
+        if (!isspace(str[i])) {
+          start = i;
+          break;
+        } 
+      }
+
+      if (start == -1) {
+        WARNING("Whitespace string trimmed!");
+        return "";
+      }
+
+      // Find the index of the last non-whitespace character
+      int end;
+      for (int i = (int)str.size() - 1; i >= 0; i--) {
+        if (!isspace(str[i])) {
+          end = i;
+          break;
+        } 
+      }
+
+      return str.substr(start, end - start + 1);
+
+    }
+
 
   }
-
-
-  /**
-   * @brief Function that joins multiple strings together by a delimeter
-   * @param strs   A std::vector of the strings to be joined
-   * @param delim  The delimeter with which the string is joined. Defaults to a single space ' '
-   * @returns      A std::string with the strings joined with the delimeter
-   */
-  inline std::string join(const std::vector<std::string> strs, char delim=' ') {
-
-    std::ostringstream stream;
-
-    for (const std::string str : strs) {
-      stream << delim << str;
-    }
-
-    return stream.str();
-
-  }
-
-
-  /**
-   * @brief Function that converts a given string to lower case
-   * @param str  Constant string pointer to the string to be lower cased
-   * @returns    A std::string object equivalent to the given string in lower case
-   */
-  inline std::string toLower(const std::string& str) {
-
-    std::string ret;
-    ret.reserve(str.size());
-    for (int i = 0; i < (int)str.size(); i++) {
-      char c = (char)tolower(str[i]);
-      ret.push_back(c);
-    }
-
-    return ret;
-
-  } 
-
-
-  /**
-   * @brief Function that converts a given string to upper case
-   * @param str  Constant string pointer to the string to be upper cased
-   * @returns    A std::string object equivalent to the given string in upper case
-   */
-  inline std::string toUpper(const std::string& str) {
-
-    std::string ret;
-    ret.reserve(str.size());
-    for (int i = 0; i < (int)str.size(); i++) {
-      char c = (char)toupper(str[i]);
-      ret.push_back(c);
-    }
-
-    return ret;
-
-  } 
-
-
-  /**
-   * @brief Function that removes the leading and trailing whitespaces from a string
-   * @param str  Constant string pointer to the string to be trimmed
-   * @returns    A std::string object equivalent to the given string without leading and trailing whitespaces
-   */
-  inline std::string trim(const std::string& str) {
-
-    if (str == "") {
-      ERROR("Cannot trim an empty string!");
-    }
-
-    // Find the index of the first non-whitespace character
-    int start = -1;
-    for (int i = 0; i < (int)str.size(); i++) {
-      if (!isspace(str[i])) {
-        start = i;
-        break;
-      } 
-    }
-
-    if (start == -1) {
-      WARNING("Whitespace string trimmed!");
-      return "";
-    }
-
-    // Find the index of the last non-whitespace character
-    int end;
-    for (int i = (int)str.size() - 1; i >= 0; i--) {
-      if (!isspace(str[i])) {
-        end = i;
-        break;
-      } 
-    }
-
-    return str.substr(start, end - start + 1);
-
-  }
-
 
 }
 
