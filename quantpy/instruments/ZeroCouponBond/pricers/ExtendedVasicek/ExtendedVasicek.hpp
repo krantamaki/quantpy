c343bed801935584ecf3e57ea344de8006c744c7
krantamaki <kasper.rantamaki@gmail.com>
2026-01-15 07:10:37 +0200

    Fixing documentation

--- quantpy/instruments/ZeroCouponBond/pricers/ExtendedVasicek/ExtendedVasicek.hpp
+++ quantpy/instruments/ZeroCouponBond/pricers/ExtendedVasicek/ExtendedVasicek.hpp
@@ -1,13 +1,20 @@
 /**
  * @file ExtendedVasicek.hpp
  * @details Both Hull and White [1] as well as Brigo and Mercurio [2] found the same extension of the 
- * Vasicek [3] model. This model is colloquially known as the extended Vasicek model. Under extended
- * Vasicek model the term structure is modelled by a stochastic process
+ * Vasicek [3] model. This model is colloquially known as the extended Vasicek model. Using the
+ * notation from [2], the interest rate \f$ r_t \f$ consists of a time-homogeneous stochastic part \f$ x_t \f$
+ * and a time-dependent deterministic function \f$ \phi_t \f$ as
  * 
- * \f$ dr_t = \theta (\mu - r_t) dt + \sigma \dW_t , \f$
+ * \f$ r_t = x_t + \phi_t . \f$
+ * 
+ * Here the stochastic process \f$ dx_t \f$ is defined as 
+ * 
+ * \f$ \theta (\mu - x_t)dt + \sigma dW_t , \f$
  * 
  * where \f$ \theta \f$ is the rate of mean-reversion, \f$ \mu \f$ is the long term mean, \f$ \sigma \f$ is the 
- * volatility and \f$ dW_t \f$ is the Brownian motion.
+ * volatility and \f$ dW_t \f$ is the Brownian motion. The short rate process is then
+ * 
+ * \f$ dr_t = \left( \theta \mu \theta \phi_t + \frac{d\phi}{dt} - \theta r_t \right) dt + \sigma [r_t - \phi_t] dW_t . \f$
  * 
  * The extended Vasicek model has a closed form solution for the price of a zero-coupon bond
  * 
