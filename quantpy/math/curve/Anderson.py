84b2a6cdb88a69ee605c57695b222686b1b3342d
krantamaki <kasper.rantamaki@gmail.com>
2026-03-09 16:31:28 +0200

    Fixes to the Anderson method

--- quantpy/math/curve/Anderson.py
+++ quantpy/math/curve/Anderson.py
@@ -17,7 +17,7 @@ __all__ = ["Anderson"]
 def _b_spline_elem(order: int, knots: List[float]) -> Callable[[float], float]:
   """A piecewise polynomial basis spline defined using De Boor recursion"""
   assert len(knots) == order + 2
-  # assert knots == sorted(knots)
+  assert (knots == sorted(knots)).all()
   
   if order == 0:
     
@@ -65,14 +65,16 @@ class Anderson(TermStructureABC):
   Bank of England Quarterly Bulletin, vol. 39, no. 4, pp. 384-392, 1999.
   """
   
-  def __init__(self, yields: np.ndarray[float], maturities: np.ndarray[float], smoothing_params: Tuple[float, float, float], n_knots: Optional[int] = None, **kwargs: Dict[any, any]) -> None:
+  def __init__(self, yields: np.ndarray[float], maturities: np.ndarray[float], smoothing_params: Tuple[float, float, float], knots: Optional[List[float]] = None) -> None:
     """
     @brief Constructor method for the Anderson term-structure model
     @param yields            Array of observed zero-coupon yields
     @param maturities        Array of year fractions corresponding with the zero-coupon yields
     @param smoothing_params  The parameters for the smoothing function defined by Anderson and Sleath. Should be of form (L, S, i)
-    @param n_knots           The number of knots used when defining the spline. Optional, defaults to len(yields) // 3
-    @param **kwargs          Optional keyword arguments. To be passed to the scipy.optimize.curve_fit function
+    @param knots             The knot points. Should be an ascending array of values which end points coincide with the minimum and maximum
+    of the 'maturities' array. Note that the "padding" is added by the constructor and is not needed in the passed array. Additionally,
+    note that unless the knot points are uniformly distributed, the solution might be challenging to find
+    Optional, defaults to using a uniformly distributed knots on the value range.
     @returns                 None
     """
     assert len(yields) == len(maturities), f"Lengths of the arrays must match! ({len(yields)} != {len(maturities)})"
@@ -80,17 +82,28 @@ class Anderson(TermStructureABC):
     self.__y = np.array(yields)
     self.__m = np.array(maturities)
     
-    self.__order   = 3
-    self.__n_knots = n_knots if n_knots is not None else int(len(yields) / 3.) + 1
-    
     m_0 = min(self.__m)
     m_n = max(self.__m)
+    
+    self.__order = 3
+    
+    if knots is not None:
+      assert (knots == sorted(knots)).all(), "The knots need to be an increasing sequence of values"
+      assert min(knots) == m_0, f"The minimum of the knots does not coincide with the minimum of the observed maturities! ({min(knots)} != {m_0})"
+      assert max(knots) == m_n, f"The maximum of the knots does not coincide with the maximum of the observed maturities! ({min(knots)} != {m_n})"
+      
+      self.__n_knots = len(knots)
+      self.__knots   = knots
+    
+    else:
+      self.__n_knots = int(len(yields) / 3.) + 1
 
-    # Define the knot vector. The knots are assumed to be evenly spaced in the range defined by the observations
-    self.__knots = np.linspace(m_0, m_n, self.__n_knots)
+      # Define the knot vector. The knots default to using evenly spaced points in the range defined by the observations
+      self.__knots = np.linspace(m_0, m_n, self.__n_knots)
     
     # Pad the knot vector
     self.__knots   = np.array([m_0] * self.__order + list(self.__knots) + [m_n] * self.__order)
+    
     self.__n_basis = len(self.__knots) - self.__order - 1
     
     # Build the basis matrix and the collect the basis splines into an array
@@ -112,12 +125,12 @@ class Anderson(TermStructureABC):
         
         func = lambda x: _b_spline_2nd_derivative(spline_i)(x) * _b_spline_2nd_derivative(spline_j)(x) * _lambda(x, *smoothing_params)
         
-        L_ij = quad(func, m_0, m_n)
+        L_ij = quad(func, m_0, m_n, epsabs=1e-6, epsrel=1e-6)
         
         self.__L[i, j] = L_ij[0]
     
     # Find the smoothing spline coefficients
-    self.__a = solve(self.__B.T @ self.__B + self.__L, self.__B.T @ self.__y)
+    self.__a = solve(self.__B.T @ self.__B + self.__L, self.__B.T @ self.__y, assume_a='sym')
       
     
   def __call__(self, maturity: Union[np.ndarray[float], float]) -> Union[np.ndarray[float], float]:
@@ -125,4 +138,9 @@ class Anderson(TermStructureABC):
       return np.array([self(m) for m in maturity])
      
     return sum([self.__a[i] * self.__spline_elems[i](maturity) for i in range(len(self.__a))])
+  
+  
+  @property
+  def params(self) -> Optional[Dict[any, any]]:
+    return None
     
